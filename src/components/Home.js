0b475d89a29f3f3c182d221c29d05331197c5fec
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2022-12-12 15:11:56 -0500

    added some styling to Nav elements on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,22 +8,19 @@ function Home(){
             <h3>The Hero's Journey according to Joseph Campbell</h3>
             <h3>examines the stages of the hero who goes on an adventure,</h3>
             <h3>faces a crisis and wins, then returns victorious.</h3>
-
-            <p>Do you want to be a hero?
-                <NavLink to="/addhero" style={{color: 'blue'}}>
-                    If so click here and be a hero today!
+            <div className="home-navlink">
+                <NavLink to="/addhero" style={{color: 'red'}}>
+                    Do you want to be a hero?  If so click here and be a hero today! 
                 </NavLink>
-            </p>
-            <p>Or would you rather see who's on the roster first?
-                <NavLink to="/displayheroes" style={{color: 'blue'}}>
-                Click to access the Superhero Codex
+                <br/>
+                <NavLink to="/displayheroes" style={{color: 'red'}}>
+                    Or would you rather see who's on the roster first?  Click to access the Superhero Codex
                 </NavLink>
-            </p>
-            <p>Maybe you just want to see some cool action
-                <NavLink to="/slideshow" style={{color: 'blue'}} >
-                    Click here, We got what you need!
+                <br/>
+                <NavLink to="/slideshow" style={{color: 'red'}} >
+                    Maybe you just want to see some cool action!  Click here, We got what you need!
                 </NavLink>
-            </p>
+            </div>   
         </div>
         )
 }
